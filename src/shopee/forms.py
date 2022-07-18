4f1da43cf76cf76236c71630032ea73cfdd188c4
PolunLin <polunlin1019@gmail.com>
2022-07-18 16:40:02 +0800

    fix:add field in adForm

--- src/shopee/forms.py
+++ src/shopee/forms.py
@@ -3,12 +3,20 @@ from matplotlib import widgets
 from .models import AdData,MetaData
 from dal import autocomplete
 
- 
+
 class AdForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AdForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'easyui-combobox'
+            visible.field.widget.attrs['name'] = visible.field.label
+            visible.field.widget.attrs['style'] = 'width:150ch'
+    keyword = forms.ModelChoiceField(label="關鍵字",queryset= AdData.objects.values_list('keyword',flat=True).distinct())
+    mode = forms.ModelChoiceField(label="模式",queryset=AdData.objects.values_list('mode',flat=True).distinct())
+    status = forms.ModelChoiceField(label=u'狀態',queryset=AdData.objects.values_list('status',flat=True).distinct())
+    search_request = forms.ModelChoiceField(label=u'搜尋请求',queryset=AdData.objects.values_list('search_request',flat=True).distinct())
     class Meta:
         model = AdData
-        fields =["keyword","status","mode","search_request","view","click","click_rate","transfer","dtransfer","transfer_rate","dtransfer_rate","cost_transfer","cost_dtransfer","sold_number","dsold_number","sold_cost","sold_dcost","cost","avg_rank","invent_rate","dinvent_rate","cost_revenue_rate","dcost_revenue_rate"]
-        widgets = {
-            'keyword':autocomplete.ListSelect2(url='shopee:addata-autocomplete',forward=['keyword'],attrs={'class':"form-control"}),
-            'mode':autocomplete.ListSelect2(url='shopee:addata-autocomplete',forward=['mode'],attrs={'class':"form-control"}),
-        }
+        fields =["keyword","status","mode","search_request"]
+        autocomplete_fields = ['keyword','mode']
+       
